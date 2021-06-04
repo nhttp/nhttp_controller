b28112d3906282d10d215feaed424e96414611b3
herudi <herudi7@gmail.com>
2021-06-04 18:16:40 +0700

    first

---
+++ README.md
@@ -0,0 +1,197 @@
+## NHttp Controller
+Micro routing controller for Deno with decorator support.
+> limited to [nhttp](https://github.com/nhttp/nhttp) only.
+
+## Installation
+### deno.land
+```ts
+import { Get } from "https://deno.land/x/nhttp_controller@0.0.1/mod.ts";
+```
+
+### nest.land
+```ts
+// Well soon
+// import { Get } from "https://x.nest.land/nhttp_controller@0.0.1/mod.ts";
+```
+
+## Usage
+```ts
+import { NHttp, RequestEvent } from "https://deno.land/x/nhttp/mod.ts";
+import { 
+    addControllers, 
+    Controller, 
+    Get,
+    Post,
+    Status 
+} from "https://deno.land/x/nhttp_controller@0.0.1/mod.ts";
+
+const app = new NHttp();
+
+@Controller("/hello")
+class HelloController {
+
+    @Get()
+    findAll() {
+        return { name: 'john' };
+    }
+
+    @Get("/:id")
+    findById({ params }: RequestEvent) {
+        return params.id;
+    }
+
+    @Status(201)
+    @Post()
+    save({ body }: RequestEvent) {
+        return body || 'body parser is required';
+    }
+}
+
+app.use('/api', addControllers([HelloController]));
+
+app.listen(3000);
+```
+
+## Decorator
+### Controller
+@Controller(path?: string).
+```ts
+...
+@Controller("/hello")
+class HelloController {...}
+...
+```
+
+### Method
+@METHOD(path?: string).
+> Support => @Get, @Post, @Put, @Delete, @Patch, @Head, @Options, @Any, @Trace, @Connect.
+```ts
+...
+@Controller("/hello")
+class HelloController {
+
+    @Get()
+    hello() {
+        return "hello";
+    }
+}
+...
+```
+
+### Status
+@Status(code: number | (rev, next) => number).
+```ts
+...
+@Controller("/hello")
+class HelloController {
+
+    @Status(201)
+    @Post()
+    save() {
+        return "Created";
+    }
+
+    @Status((rev) => {
+        // logic here
+        return 200;
+    })
+    @Put('/:id')
+    update() {
+        return "Updated";
+    }
+}
+...
+```
+
+### Header
+@Header(object | (rev, next) => object).
+```ts
+...
+@Controller("/hello")
+class HelloController {
+
+    @Header({ "Content-Type": "text/html" })
+    @Get()
+    hello() {
+        return "<h1>Hello</h1>";
+    }
+
+    @Header((rev) => {
+        let type = rev.url.includes(".css") ? "text/css" : "text/plain";
+        return { "Content-Type": type };
+    })
+    @Get()
+    hello2() {
+        return Deno.readFile('./path/to/file');
+    }
+}
+...
+```
+### Middlewares
+@Wares(...middlewares).
+```ts
+...
+@Controller("/hello")
+class HelloController {
+
+    @Wares((rev, next) => {
+        rev.foo = "foo";
+        next();
+    })
+    @Get()
+    hello({ foo }: RequestEvent) {
+        return foo;
+    }
+}
+...
+```
+### Inject 
+@Inject(value: any, ...args: any).
+```ts
+...
+
+class HelloService {
+
+    async findAll() {
+        const data = await db.findAll();
+        return { data, status: 200 }; 
+    }
+
+    async save(body) {
+        await db.save(body);
+        return { message: 'success save', status: 201 }; 
+    }
+}
+
+@Controller("/hello")
+class HelloController {
+
+    @Inject(HelloService)
+    private readonly service!: HelloService;
+
+    @Get()
+    findAll() {
+        return this.service.findAll();
+    }
+
+    @Status(201)
+    @Post()
+    save({ body }: RequestEvent) {
+        return this.service.save(body);
+    }
+}
+...
+```
+### addControllers 
+addControllers(classControllers: class[]).
+```ts
+...
+app.use(addControllers([HelloController, OtherController]));
+app.use('/api/v1', addControllers([HelloController, OtherController]));
+app.use('/api/v1', middleware(), addControllers([HelloController, OtherController]));
+...
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ mod.ts
@@ -0,0 +1,175 @@
+import {
+  Handler,
+  Handlers,
+  JsonResponse,
+  NextFunction,
+  RequestEvent,
+  Router,
+} from "https://deno.land/x/nhttp@0.1.1/mod.ts";
+
+type TStatus<
+  Rev extends RequestEvent = RequestEvent,
+> = (
+  rev: Rev,
+  next: NextFunction,
+) => number;
+
+type THeaders<
+  Rev extends RequestEvent = RequestEvent,
+> = (
+  rev: Rev,
+  next: NextFunction,
+) => { [k: string]: any };
+
+function findFns(arr: any[]): any[] {
+  let ret = [] as any, i = 0, len = arr.length;
+  for (; i < len; i++) {
+    if (Array.isArray(arr[i])) ret = ret.concat(findFns(arr[i]));
+    else if (typeof arr[i] === "function") ret.push(arr[i]);
+  }
+  return ret;
+}
+
+function joinTargetMethod(target: any, prop: string, arr: any[]) {
+  let obj = target["methods"] || {};
+  obj[prop] = obj[prop] || {};
+  obj[prop].handlers = arr.concat(obj[prop].handlers || []);
+  return obj;
+}
+
+function addMethod(method: string, path: string = "") {
+  return (target: any, prop: string, des: PropertyDescriptor) => {
+    const ori = des.value;
+    des.value = function (...args: any[]) {
+      let result = ori.apply(target, args);
+      return result;
+    };
+    let obj = target["methods"] || {};
+    obj[prop] = obj[prop] || {};
+    let handlers = (obj[prop].handlers || []).concat([wrapFn(des.value)]);
+    obj[prop] = { path, method, handlers };
+    target["methods"] = obj;
+    return des;
+  };
+}
+
+export function Wares<
+  Rev extends RequestEvent = RequestEvent,
+>(...middlewares: Handlers<Rev>) {
+  let fns = findFns(middlewares);
+  return (target: any, prop: string, des: PropertyDescriptor) => {
+    target["methods"] = joinTargetMethod(target, prop, fns);
+    return des;
+  };
+}
+
+export function Status(status: number | TStatus) {
+  return (target: any, prop: string, des: PropertyDescriptor) => {
+    const statusFn: Handler = (rev, next) => {
+      rev.responseInit = rev.responseInit || {};
+      rev.responseInit.status = typeof status === "function"
+        ? status(rev, next)
+        : status;
+      next();
+    };
+    target["methods"] = joinTargetMethod(target, prop, [statusFn]);
+    return des;
+  };
+}
+
+export function Header(header: { [k: string]: any } | THeaders) {
+  return (target: any, prop: string, des: PropertyDescriptor) => {
+    const headerFn: Handler = (rev, next) => {
+      let obj = typeof header === "function" ? header(rev, next) : header;
+      rev.responseInit = rev.responseInit || {};
+      let headers = rev.responseInit.headers || new Headers();
+      for (const key in obj) headers.set(key, obj[key]);
+      rev.responseInit.headers = headers;
+      next();
+    };
+    target["methods"] = joinTargetMethod(target, prop, [headerFn]);
+    return des;
+  };
+}
+
+export function Inject(value: any, ...args: any) {
+  return function (target: any, prop: string) {
+    target[prop] = typeof value === "function" ? new value(...args) : value;
+  };
+}
+
+export const Get = (path: string = "") => addMethod("GET", path);
+export const Post = (path: string = "") => addMethod("POST", path);
+export const Put = (path: string = "") => addMethod("PUT", path);
+export const Delete = (path: string = "") => addMethod("DELETE", path);
+export const Any = (path: string = "") => addMethod("ANY", path);
+export const Options = (path: string = "") => addMethod("OPTIONS", path);
+export const Head = (path: string = "") => addMethod("HEAD", path);
+export const Trace = (path: string = "") => addMethod("TRACE", path);
+export const Connect = (path: string = "") => addMethod("CONNECT", path);
+export const Patch = (path: string = "") => addMethod("PATCH", path);
+
+export function Controller(path: string = "") {
+  return (target: Function) => {
+    let c_routes = [] as any[];
+    let obj = target.prototype["methods"];
+    for (const k in obj) {
+      if (path !== "") obj[k].path = path + obj[k].path;
+      c_routes.push(obj[k]);
+    }
+    target.prototype.c_routes = c_routes;
+    delete target.prototype["methods"];
+  };
+}
+
+function retBody(body: any, rev: RequestEvent) {
+  let send = (x: any) => rev.respondWith(new Response(x, rev.responseInit));
+  let json = (y: any) => rev.respondWith(new JsonResponse(y, rev.responseInit));
+  if (typeof body === "string") return send(body);
+  if (typeof body === "object") {
+    if (typeof body.then === "function") return withPromise(body, rev);
+    if (
+      body instanceof Uint8Array ||
+      body instanceof ReadableStream ||
+      typeof (body as Deno.Reader).read === "function"
+    ) {
+      return send(body);
+    }
+    return json(body);
+  }
+  return void 0;
+}
+
+function wrapFn(handler: Handler): Handler {
+  return (rev, next) => retBody(handler(rev, next), rev);
+}
+
+async function withPromise(
+  handler: Promise<Handler>,
+  rev: RequestEvent,
+): Promise<any> {
+  let body = await handler;
+  return retBody(body, rev);
+}
+
+class AddController extends Router {
+  constructor(arr: { new (): any }[]) {
+    super();
+    let i = 0, len = arr.length, routes = this.c_routes;
+    while (i < len) {
+      routes = routes.concat(arr[i].prototype.c_routes);
+      ++i;
+    }
+    this.c_routes = routes;
+  }
+}
+
+export function nControllerInit(): Handler {
+  return (rev, next) => {
+    rev.responseInit = {};
+    next();
+  };
+}
+
+export const addControllers = (controllers: { new (...args: any): any }[]) =>
+  new AddController(controllers);
